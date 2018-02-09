3a4205472db960c0a4cd707cdb9c3485884cdee8
Steven Novak <stevenrnovak@gmail.com>
2018-02-09 09:47:41 -0800

    Initial commit with simple root controller

---
+++ CAApi/Controllers/RootController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CAApi
+{
+    [Route("/")]
+    public class RootController : Controller
+    {
+        [HttpGet(Name = nameof(GetRoot))]
+        public IActionResult GetRoot()
+        {
+            var response = new
+            {
+                href = Url.Link(nameof(GetRoot), null)
+            };
+
+            return Ok(response);
+        }
+    }
+}

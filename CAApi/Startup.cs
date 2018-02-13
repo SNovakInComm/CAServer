a2f56901d05a9188a04f7b63467aba92f65b65a1
credential.helper <33043784+SNovakInComm@users.noreply.github.com>
2018-02-13 15:29:02 -0800

    mild changes to .gitignore and CRLF

--- CAApi/Startup.cs
+++ CAApi/Startup.cs
@@ -1,78 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Microsoft.AspNetCore.Mvc.Formatters;
-using CAApi.Infrastructure;
-using Microsoft.AspNetCore.Mvc.Versioning;
-using Microsoft.AspNetCore.Mvc;
-using CAApi.Filters;
-
-namespace CAApi
-{
-    public class Startup
-    {
-        private readonly int? _httpsPort;
-
-        public Startup(IConfiguration configuration, IHostingEnvironment env)
-        {
-            Configuration = configuration;
-
-            if(env.IsDevelopment())
-            {
-                var lauchJsonConfig = new ConfigurationBuilder()
-                    .SetBasePath(env.ContentRootPath)
-                    .AddJsonFile($"Properties{System.IO.Path.DirectorySeparatorChar}launchSettings.json")
-                    .Build();
-                _httpsPort = lauchJsonConfig.GetValue <int> ("iissettings:iisexpress:sslport");
-            }
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc( opt =>
-            {
-                opt.Filters.Add(typeof(JsonExceptionFilter));
-
-                opt.SslPort = _httpsPort;
-                opt.Filters.Add(typeof(RequireHttpsAttribute));
-
-                var jsonFormatter = opt.OutputFormatters.OfType<JsonOutputFormatter>().Single();
-                opt.OutputFormatters.Remove(jsonFormatter);
-                opt.OutputFormatters.Add(new IonOutputFormatter(jsonFormatter));
-            });
-
-            services.AddRouting( opt => opt.LowercaseUrls = true );
-
-            services.AddApiVersioning( opt =>
-            {
-                opt.ApiVersionReader = new MediaTypeApiVersionReader();
-                opt.AssumeDefaultVersionWhenUnspecified = true;
-                opt.ReportApiVersions = true;
-                opt.DefaultApiVersion = new ApiVersion(1, 0);
-                opt.ApiVersionSelector = new CurrentImplementationApiVersionSelector(opt);
-            });
-
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseMvc();
-        }
-    }
-}
+﻿using System;using System.Collections.Generic;using System.Linq;using System.Threading.Tasks;using Microsoft.AspNetCore.Builder;using Microsoft.AspNetCore.Hosting;using Microsoft.Extensions.Configuration;using Microsoft.Extensions.DependencyInjection;using Microsoft.Extensions.Logging;using Microsoft.Extensions.Options;using Microsoft.AspNetCore.Mvc.Formatters;using CAApi.Infrastructure;using Microsoft.AspNetCore.Mvc.Versioning;using Microsoft.AspNetCore.Mvc;using CAApi.Filters;namespace CAApi{    public class Startup    {        private readonly int? _httpsPort;        public Startup(IConfiguration configuration, IHostingEnvironment env)        {            Configuration = configuration;            if(env.IsDevelopment())            {                var lauchJsonConfig = new ConfigurationBuilder()                    .SetBasePath(env.ContentRootPath)                    .AddJsonFile($"Properties{System.IO.Path.DirectorySeparatorChar}launchSettings.json")                    .Build();                _httpsPort = lauchJsonConfig.GetValue <int> ("iissettings:iisexpress:sslport");            }        }        public IConfiguration Configuration { get; }        // This method gets called by the runtime. Use this method to add services to the container.        public void ConfigureServices(IServiceCollection services)        {            services.AddMvc( opt =>            {                opt.Filters.Add(typeof(JsonExceptionFilter));                opt.SslPort = _httpsPort;                opt.Filters.Add(typeof(RequireHttpsAttribute));                var jsonFormatter = opt.OutputFormatters.OfType<JsonOutputFormatter>().Single();                opt.OutputFormatters.Remove(jsonFormatter);                opt.OutputFormatters.Add(new IonOutputFormatter(jsonFormatter));            });            services.AddRouting( opt => opt.LowercaseUrls = true );            services.AddApiVersioning( opt =>            {                opt.ApiVersionReader = new MediaTypeApiVersionReader();                opt.AssumeDefaultVersionWhenUnspecified = true;                opt.ReportApiVersions = true;                opt.DefaultApiVersion = new ApiVersion(1, 0);                opt.ApiVersionSelector = new CurrentImplementationApiVersionSelector(opt);            });        }        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.        public void Configure(IApplicationBuilder app, IHostingEnvironment env)        {            if (env.IsDevelopment())            {                app.UseDeveloperExceptionPage();            }            app.UseMvc();        }    }}
